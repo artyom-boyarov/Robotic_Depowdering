dc4d8e45b41d2654c7199a9acc713c86d848fc27
Artyom Boyarov <artyom.boyarov@gmail.com>
2024-07-16 01:22:24 -0400

    Added calculation of quaternion for target grasp pose orientation

--- src/robotic_depowdering/src/pick_up_object.cpp
+++ src/robotic_depowdering/src/pick_up_object.cpp
@@ -22,6 +22,8 @@ int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
 
     const auto logger = rclcpp::get_logger("rclcpp");
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("pick_up_object_client");
+
 
     // Assume all arguments are correctly formed.
     // Will have to change this.
@@ -82,14 +84,21 @@ int main(int argc, char** argv) {
     target_pose.position.x = grasp_config->getPosition().x();
     target_pose.position.y = grasp_config->getPosition().y();
     target_pose.position.z = grasp_config->getPosition().z();
-    Eigen::Quaternion<double> rot = Eigen::Quaternion<double>::FromTwoVectors(Eigen::Vector3d(0.0,0.0,1.0), grasp_config->getApproach());
-    target_pose.orientation.w = rot.w();
-    target_pose.orientation.x = rot.x();
-    target_pose.orientation.y = rot.y();
-    target_pose.orientation.z = rot.z();
+    Eigen::Matrix3d rot_mat;
+    rot_mat << grasp_config->getAxis().x(), grasp_config->getBinormal().x(), grasp_config->getApproach().x(),
+           grasp_config->getAxis().y(), grasp_config->getBinormal().y(), grasp_config->getApproach().y(),
+           grasp_config->getAxis().z(), grasp_config->getBinormal().z(), grasp_config->getApproach().z();
+    Eigen::Quaternion<double> rot_q(rot_mat);
+    rot_q.normalize();
+    target_pose.orientation.w = rot_q.w();
+    target_pose.orientation.x = rot_q.x();
+    target_pose.orientation.y = rot_q.y();
+    target_pose.orientation.z = rot_q.z();
 
-    RCLCPP_INFO(logger, "Got quaternion w:%f x:%f y:%f z:%f", rot.w(), rot.x(), rot.y(), rot.z());
-
+    RCLCPP_INFO(logger, "Got quaternion w:%f x:%f y:%f z:%f", rot_q.x(), rot_q.y(), rot_q.z(), rot_q.w());
+    RCLCPP_INFO(logger, "%f %f %f %f %f %f %f",
+        grasp_config->getPosition().x(), grasp_config->getPosition().y(), grasp_config->getPosition().z(),
+        rot_q.x(), rot_q.y(), rot_q.z(), rot_q.w());
     // Create a service client to call the move_to_pose service
     // auto client = node->create_client<rizon_pick_and_place::srv::MoveToPose>("move_to_pose");
     // while (!client->wait_for_service(std::chrono::seconds(1))) {
@@ -113,119 +122,6 @@ int main(int argc, char** argv) {
     // } else {
     //     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service move_to_pose");
     // }
-
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\tposition: <%f, %f, %f>", 
-        grasp_config->getPosition().x(), grasp_config->getPosition().y(), grasp_config->getPosition().z());
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\tapproach: <%f, %f, %f>", 
-        grasp_config->getApproach().x(), grasp_config->getApproach().y(), grasp_config->getApproach().z());
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\tbinormal: <%f, %f, %f>", 
-        grasp_config->getBinormal().x(), grasp_config->getBinormal().y(), grasp_config->getBinormal().z());
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\taxis: <%f, %f, %f>", 
-        grasp_config->getAxis().x(), grasp_config->getAxis().y(), grasp_config->getAxis().z());
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\taperture: %f", 
-        grasp_config->getGraspWidth());
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\tscore: %f", 
-        grasp_config->getScore());
-    
-
-
-    // Carry out IK 
-    //   \/\/\/\/
-    // This simply sets random positions.
-    std::vector<double> positions1 = {-1.40, -0.70, 0.00, 1.57, 0.01, 0.69, 0.00};
-    std::vector<double> positions2 = {-1.81, -0.70, 0.00, 1.57, 0.01, 0.69, 0.00};
-    // srand((unsigned int)time(NULL));
-    // for (auto& pos : positions) {
-    //     pos = (float)rand() / RAND_MAX;
-    // }
-    
-
-    // Now, after we've done IK, we publish the list of joint angles for the 7 joints to "/rizon_arm_controller/joint_trajectory".
-    const std::string rizon_interface_topic = "/rizon_arm_controller/joint_trajectory";
-    rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr jointTrajectoryPublisher = 
-        node->create_publisher<trajectory_msgs::msg::JointTrajectory>(rizon_interface_topic, 10);
-    
-    trajectory_msgs::msg::JointTrajectory jointTrajectoryMessage;
-    jointTrajectoryMessage.joint_names = std::vector<std::string>({
-      "joint1",
-      "joint2",
-      "joint3",
-      "joint4",
-      "joint5",
-      "joint6",
-      "joint7"});
-    
-    trajectory_msgs::msg::JointTrajectoryPoint armPoint;
-    armPoint.positions = positions1;
-    auto duration = builtin_interfaces::msg::Duration();
-    duration.sec = 1;
-    armPoint.time_from_start = duration;
-
-    jointTrajectoryMessage.points = {armPoint};
-
-    try {
-        jointTrajectoryPublisher->publish(jointTrajectoryMessage);
-        rclcpp::spin_some(node);
-    } catch (const rclcpp::exceptions::RCLError& e) {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to send message to Rizon 4s. Details: %s", e.what());
-    }
-
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sent the arm to go to <%f, %f, %f, %f, %f, %f, %f>", 
-        positions1[0], 
-        positions1[1], 
-        positions1[2], 
-        positions1[3], 
-        positions1[4], 
-        positions1[5], 
-        positions1[6]
-    );
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Waiting for 5 seconds");
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-
-    armPoint.positions = positions2;
-    jointTrajectoryMessage.points = {armPoint};
-    try {
-        jointTrajectoryPublisher->publish(jointTrajectoryMessage);
-        rclcpp::spin_some(node);
-    } catch (const rclcpp::exceptions::RCLError& e) {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to send message to Rizon 4s. Details: %s", e.what());
-    }
-
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sent the arm to go to <%f, %f, %f, %f, %f, %f, %f>", 
-        positions2[0], 
-        positions2[1], 
-        positions2[2], 
-        positions2[3], 
-        positions2[4], 
-        positions2[5], 
-        positions2[6]
-    );
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Waiting for 5 seconds");
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-
-    
-    armPoint.positions = positions1;
-    jointTrajectoryMessage.points = {armPoint};
-
-    try {
-        jointTrajectoryPublisher->publish(jointTrajectoryMessage);
-        rclcpp::spin_some(node);
-    } catch (const rclcpp::exceptions::RCLError& e) {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to send message to Rizon 4s. Details: %s", e.what());
-    }
-
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sent the arm to go to <%f, %f, %f, %f, %f, %f, %f>", 
-        positions1[0], 
-        positions1[1], 
-        positions1[2], 
-        positions1[3], 
-        positions1[4], 
-        positions1[5], 
-        positions1[6]
-    );
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Waiting for 5 seconds");
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-
 
     rclcpp::shutdown();
     return 0;
